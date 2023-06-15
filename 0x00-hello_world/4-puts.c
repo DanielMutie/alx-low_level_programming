0a1ae279c67a10c0906f9a9e9d668182e1836435
DanielMutie <dancunrandy@hotmail.com>
2023-06-15 10:44:24 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - program that prints put function
+ * Return: 0 (Executed)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
