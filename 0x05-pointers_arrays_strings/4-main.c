9c28aad33a4a29248bc3a21658883d146d32ba14
DanielMutie <dancunrandy@hotmail.com>
2023-06-27 11:49:21 -0700

    4-main.c

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point of the program.
+ *        Tests the print_rev function.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	print_rev(str);
+	return (0);
+}
+
