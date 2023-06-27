ab377b817f72424c4f17dc256765c97ea972c337
DanielMutie <dancunrandy@hotmail.com>
2023-06-27 11:58:03 -0700

    6-main.c

---
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program.
+ *        Tests the puts2 function.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "0123456789";
+    puts2(str);
+    return (0);
+}
+
