3499c4df5c601c2c25bdb8d8c9c00df1efd387e2
DanielMutie <dancunrandy@hotmail.com>
2023-06-27 11:42:09 -0700

    3-main.c

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point of the program.
+ *        Tests the _puts function.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
+
