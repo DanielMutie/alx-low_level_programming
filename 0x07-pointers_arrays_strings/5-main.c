069f52dca43bc54f32c6d3e65e37da324cf09f1b
DanielMutie <dancunrandy@hotmail.com>
2023-07-04 11:41:41 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-main.c
+++ 0x07-pointers_arrays_strings/5-main.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- * main - Check the code.
+ * main - check the code
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,9 +1,10 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strstr - Locates a substring.
- * @haystack: Pointer to the string to search in.
- * @needle: Pointer to the substring to locate.
+ * @haystack: The string to search in.
+ * @needle: The substring to find.
  *
  * Return: A pointer to the beginning of the located substring,
  *         or NULL if the substring is not found.
@@ -17,15 +18,33 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] != '\0'; j++)
+		j = 0;
+		while (needle[j] != '\0')
 		{
 			if (haystack[i + j] != needle[j])
 				break;
+			j++;
 		}
 		if (needle[j] == '\0')
-			return (haystack + i);
+			return (&haystack[i]);
 	}
 
 	return (NULL);
+}
+
+/**
+ * main - Check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *s = "hello, world";
+	char *f = "world";
+	char *t;
+
+	t = _strstr(s, f);
+	printf("%s\n", t);
+	return (0);
 }
 
